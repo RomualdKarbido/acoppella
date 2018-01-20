ee0db85e24b3dfd8a896353d73504bafffe09690
romuald <rarsenev@usetech.ru>
2018-01-20 11:08:27 +0300

    прогресс

--- build/scripts/scripts.js
+++ build/scripts/scripts.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).ready(function() {
 	// слайдер
 	$(".slider-1").slick({
 		slidesToShow: 3,
@@ -6,8 +6,35 @@ $(function(){
 		dots: false,
 		arrows: true,
 		autoplay: false, 
+	});
+
+	// меню
+	$('.top-menu').click(function() {
+		$(this).toggleClass('active');
+		$('.top-menu__dropbox').toggleClass('active');
+	});
+	$('.top-menu__item').click(function() {
+		$('.top-menu__item').removeClass('active');
+		$(this).addClass('active');
+
+		setTimeout(function() {
+			$('.top-menu__dropbox').removeClass('active');
+			$('.top-menu').removeClass('active');
+
+		}, 1000);
 		
 	});
+
+	
+	$(".top-menu__item").click(function() {
+      $("html, body").animate({
+         scrollTop: $($(this).attr("href")).offset().top + "px"
+      }, {
+         duration: 500,
+         easing: "swing"
+      });
+      return false;
+   });
 
 
 })
