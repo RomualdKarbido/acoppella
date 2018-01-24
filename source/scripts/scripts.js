a847578fcc5b299277121a36378bf31737ecb5c6
romuald <rarsenev@usetech.ru>
2018-01-24 03:42:48 +0300

    новые правки

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
 	// слайдер
 	$(".slider-1").slick({
@@ -11,8 +13,14 @@ $(document).ready(function() {
 	// меню
 	$('.top-menu').click(function() {
 		$(this).toggleClass('active');
+		$('.top-menu').removeClass('white');
 		$('.top-menu__dropbox').toggleClass('active');
 		$('.top-menu__item').toggleClass('animated bounceInDown');
+
+		
+		
+			
+			
 	});
 	$('.top-menu__item').click(function() {
 		$('.top-menu__item').removeClass('active');
@@ -28,7 +36,7 @@ $(document).ready(function() {
 	});
 
 	//плавный скроллинг
-	$(".top-menu__item").click(function() {
+	$(".top-menu__item, .btn_top").click(function() {
       $("html, body").animate({
          scrollTop: $($(this).attr("href")).offset().top + "px"
       }, {
@@ -37,7 +45,34 @@ $(document).ready(function() {
       });
       return false;
    });
-
-	
+		
 
 })
+
+
+// if($('.top-menu__dropbox').hasClass('active')) {
+// 			console.log('2323');
+
+			
+$(window).on('load scroll', function() { 
+	var winH = window.innerHeight;
+	console.log(winH);
+
+
+    if (
+    	$(this).scrollTop() >= winH - 100 
+    	&& $(this).scrollTop() <= winH *2 - 100
+    	|| $(this).scrollTop() >= winH *3 - 100
+    	&& $(this).scrollTop() <= winH *4 - 100
+    	|| $(this).scrollTop() >= winH *5 - 100
+    	&& $(this).scrollTop() <= winH *6 - 100
+
+    ){
+        $('.top-menu').addClass('white');
+
+    }
+    else {
+    	$('.top-menu').removeClass('white');
+    }
+});
+
